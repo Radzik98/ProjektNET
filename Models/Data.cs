6d26185abbc47db199c5198337dad789d5945e6c
Piotr Zamora <peterzzam@gmail.com>
2022-06-04 00:43:22 +0200

    It's finally working however not as expected

--- Models/Data.cs
+++ Models/Data.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ProjektNET.Data
 {
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using ProjektNET.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProjektNET.Pages
 {
