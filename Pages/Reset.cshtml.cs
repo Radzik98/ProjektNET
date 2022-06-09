3703af0397bf1010ffa2d9afa448c142eb4e9476
Piotr Zamora <peterzzam@gmail.com>
2022-06-09 17:52:33 +0200

    Dodane wszystko z mastera

---
+++ Pages/Reset.cshtml.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjektNET.Data;
+using ProjektNET.Services;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using ProjektNET.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+ 
+namespace ProjektNET.Pages
+{
+    [AllowAnonymous]
+    public class ResetModel : PageModel
+    {
+        private UserManager<User> userManager;
+
+        private readonly UserDbContext Db;
+ 
+        public ResetModel(UserDbContext Db)
+        {
+            this.Db = Db;
+        }
+ 
+        [BindProperty]
+        public InputModel Input { get; set; }
+ 
+        public string ReturnUrl { get; set; }
+ 
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+ 
+        }
+ 
+        
+        public async Task OnGetAsync(string? returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/"); 
+            ReturnUrl = returnUrl;
+        }
+ 
+        public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+ 
+            if (ModelState.IsValid)
+            {
+                var user = Db.User.Where(f => f.Email == Input.Email).FirstOrDefault();
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid Email");
+                    return Page();
+                }
+                
+                var link = "https://localhost:7222/ResetPassword?id=" + user.Id;
+
+                //var link = Url.Action("ResetPassword", null, new { email = user.Email }, Request.Scheme);
+                EmailSender emailSender = new EmailSender();
+                bool emailResponse = emailSender.SendEmailPasswordReset(user.Email, link);
+    
+                if (emailResponse)
+                    return Redirect("/ResetConfirmation");
+                else
+                {
+                    // log email failed 
+                }
+                return Page();
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ Pages/ResetPassword.cshtml.cs
@@ -0,0 +1,80 @@
+using ProjektNET.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ProjektNET.Pages
+{
+    //[Authorize]
+    public class ResetPasswordModel : PageModel
+    {
+        private readonly ProjektNET.Data.UserDbContext _context;
+
+        public ResetPasswordModel(ProjektNET.Data.UserDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User? User { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.User.FirstOrDefaultAsync(m => m.Id == id);
+            
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (User != null)
+            {
+
+                _context.Attach(User).State = EntityState.Modified;
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToPage("./ResetPasswordCofirmation", new { email = User.Email });
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(User.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool UserExists(int? id)
+        {
+            return _context.User.Any(e => e.Id == id);
+        }
+    }
+}
